08ffa95c11c0c50b469e720a83864b2f5d7add1d
Kris Tina <kristyguerrero20@gmail.com>
2021-10-06 21:12:34 -0500

    added the app uses to the server.js file

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
