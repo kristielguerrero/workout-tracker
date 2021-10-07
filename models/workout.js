2baa7d2ba28d3828b2c49584000ccff0cf2b1d9f
Kris Tina <kristyguerrero20@gmail.com>
2021-10-06 19:10:51 -0500

    added the workout schema

--- models/workout.js
+++ models/workout.js
@@ -5,4 +5,35 @@ const workoutSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Enter exercise type',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter exercise name',
+      },
+
+      duration: {
+        type: Number,
+        required: 'Enter exercise duration (minutes)',
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
