1127f3b4d752905007d210f333a9527485c403d3
Kris Tina <kristyguerrero20@gmail.com>
2021-10-06 21:40:17 -0500

    finished routes in apiRoutes file

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,2 +1,25 @@
 const app = require('express').Router();
 const db = require('../models');
+
+router.get('/', async (req, res) => {
+  db.Workout.find({}).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
+});
+
+app.post('/api/workouts', (req, res) => {
+  db.Workout.create(req.body).then((Workoutdb) => {
+    res.json(Workoutdb);
+  });
+});
+
+app.put('/api/workouts/:id', (req, res) => {
+  db.Workout.update(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  ).then((workoutdb) => {
+    re.json(workoutdb);
+  });
+});
+
+module.exports = app;
